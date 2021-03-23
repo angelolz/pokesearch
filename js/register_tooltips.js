005b153eff97032e3788916e98ac3717b76f2a07
Angel Legaspi <angellegaspi@u.boisestate.edu>
2021-03-22 23:56:23 -0600

    removed debug messages in js

--- js/register_tooltips.js
+++ js/register_tooltips.js
@@ -1,23 +1,19 @@
 function usernameShowTip()
 {
     document.getElementById("username-tooltip").style.visibility = "visible";
-    console.log("showing user tooltip");
 }
 
 function usernameHideTip()
 {
     document.getElementById("username-tooltip").style.visibility = "hidden";
-    console.log("hiding user tooltip");
 }
 
 function passwordShowTip()
 {
     document.getElementById("password-tooltip").style.visibility = "visible";
-    console.log("showing usertooltip");
 }
 
 function passwordHideTip()
 {
     document.getElementById("password-tooltip").style.visibility = "hidden";
-    console.log("hiding user tooltip");
 }
