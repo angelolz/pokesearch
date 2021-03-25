4f66c338ded68fde68af1aa56c8055d41bcf3bd3
Angel Legaspi <angellegaspi@u.boisestate.edu>
2021-03-25 01:13:52 -0600

    added functions path

--- init.php
+++ init.php
@@ -5,6 +5,7 @@ define("PRIVATE_PATH", dirname(__FILE__) . '/private');
 define("LAYOUTS_PATH", PROJECT_PATH . '/layouts');
 
 define("CLASSES_PATH", PRIVATE_PATH . '/classes');
+define("FUNCTIONS_PATH", PRIVATE_PATH . '/functions');
 define("HANDLERS_PATH", PRIVATE_PATH . '/handlers');
 define("LOG_PATH", PRIVATE_PATH . "/logs");
 ?>
