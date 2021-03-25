f9c4eb747282513402fdfc93677a5647bd127d60
Angel Legaspi <angellegaspi@u.boisestate.edu>
2021-03-25 01:13:41 -0600

    added listing for items, pokemon and moves

--- items_list.php
+++ items_list.php
@@ -5,10 +5,10 @@ require_once FUNCTIONS_PATH . "/createPagination.php";
 
 <html>
     <head>
-        <title>Pokésearch | Pokémon</title>
+        <title>Pokésearch | Items</title>
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <link rel="stylesheet" type="text/css" href="css/pagination.css">
-        <link rel="stylesheet" type="text/css" href="css/pokemon_list.css">
+        <link rel="stylesheet" type="text/css" href="css/items_list.css">
         <script src="js/rpp.js"></script>
         <?php require_once 'layouts/favicon.php'; ?>
     </head>
@@ -39,34 +39,34 @@ require_once FUNCTIONS_PATH . "/createPagination.php";
                     $rpp = 25;
                 }
 
-                $pkmnList = file_get_contents(sprintf("https://pokeapi.co/api/v2/pokemon-species?offset=%u&limit=%u", (($page - 1) * $rpp), $rpp));
-                $pkmnList = json_decode($pkmnList);
-                $maxPages = ceil($pkmnList->count / $rpp);
+                $itemList = file_get_contents(sprintf("https://pokeapi.co/api/v2/item?offset=%u&limit=%u", (($page - 1) * $rpp), $rpp));
+                $itemList = json_decode($itemList);
+                $maxPages = ceil($itemList->count / $rpp);
                 ?>
-                <h1>Pokémon</h1>
+                <h1>Items</h1>
                 <div class="nav">
-                    <?php createPagination($page, $rpp, $maxPages, $pkmnList->count, array(25,50,100)); ?>
+                    <?php createPagination($page, $rpp, $maxPages, $itemList->count, array(25,50,100)); ?>
                 </div>
                 <div class="list">
                     <?php
-                        if(count($pkmnList->results) == 0)
+                        if(count($itemList->results) == 0)
                         {
                             echo '<h3>No results found!</h3>';
                         }
-                        for($i = 1; $i <= count($pkmnList->results); $i++)
-                        {
-                            $name = ucfirst($pkmnList->results[($i - 1)]->name);
-                            $no = $i + (($page - 1) * $rpp);
-                            $img = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" . $no . ".png";
 
-                            echo '<span class="pokemon">';
-                            echo '<a href="pokemon.php?id=' . $no .'">';
+                        for($i = 1; $i <= count($itemList->results); $i++)
+                        {
+                            $name = ucwords(str_replace("-", " ", $itemList->results[($i - 1)]->name));
+                            $no = $i + (($page - 1) * $rpp);
+                            $img = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/" . $itemList->results[($i - 1)]->name . ".png";
+
+                            echo '<span class="item">';
+                            echo '<a href="item.php?id=' . $no .'">';
                             echo '<span class="icon">';
                             echo '<img src="' . $img . '"/>';
                             echo '</span>';
                             echo '<span class="info">';
                             echo "<h3>{$name}</h3>";
-                            echo "<h3>#{$no}</h3>";
                             echo '</span>';
                             echo '</a>';
                             echo '</span>';
@@ -74,7 +74,7 @@ require_once FUNCTIONS_PATH . "/createPagination.php";
                     ?>
                 </div>
                 <div class="nav">
-                    <?php createPagination($page, $rpp, $maxPages, $pkmnList->count, array(25,50,100)); ?>
+                    <?php createPagination($page, $rpp, $maxPages, $itemList->count, array(25,50,100)); ?>
                 </div>
             </span>
         </div>
